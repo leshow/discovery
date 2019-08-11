b4eeefb6b1b93182145a83d9f93f2a24c5c227bc
Evan Cameron <cameron.evan@gmail.com>
2019-08-11 15:47:30 -0400

    Echo server impl

--- src/11-usart/src/main.rs
+++ src/11-usart/src/main.rs
@@ -1,3 +1,4 @@
+#![allow(unused_macros)]
 #![deny(unsafe_code)]
 #![no_main]
 #![no_std]
@@ -5,6 +6,7 @@
 #[allow(unused_imports)]
 use aux11::{entry, iprint, iprintln, usart1};
 use core::fmt::{self, Write};
+use heapless::{consts, Vec};
 
 macro_rules! uprint {
     ($serial:expr, $($arg:tt)*) => ($serial.write_fmt(format_args!($($arg)*)).ok());
@@ -33,6 +35,29 @@ impl Write for SerialPort {
     }
 }
 
+struct Echo {
+    usart1: &'static mut usart1::RegisterBlock,
+}
+
+impl Echo {
+    pub fn read_byte(&mut self) -> u8 {
+        while self.usart1.isr.read().rxne().bit_is_clear() {}
+        self.usart1.rdr.read().rdr().bits() as u8
+    }
+    pub fn write_byte(&mut self, data: u8) {
+        while self.usart1.isr.read().txe().bit_is_clear() {}
+        self.usart1.tdr.write(|w| w.tdr().bits(u16::from(data)))
+    }
+    pub fn write<'a, I: 'a>(&mut self, data: I)
+    where
+        I: Iterator<Item = &'a u8>,
+    {
+        for &b in data {
+            self.write_byte(b);
+        }
+    }
+}
+
 #[entry]
 fn main() -> ! {
     let (usart1, mono_timer, mut itm) = aux11::init();
@@ -41,12 +66,28 @@ fn main() -> ! {
     // let mut serial = SerialPort { usart1 };
     // uprintln!(serial, "Hello friend {}", 13 + 3);
 
-    // receive
-    loop {
+    // echo server
+    let mut echo = Echo { usart1 };
+    let mut buf: Vec<u8, consts::U32> = Vec::new();
+    'outer: loop {
+        buf.clear();
         // busy wait on rxne register -- tells us that
-        while usart1.isr.read().rxne().bit_is_clear() {}
-        let data = usart1.rdr.read().rdr().bits() as u8;
-        aux11::bkpt();
+        'inner: loop {
+            let data = echo.read_byte();
+            iprintln!(&mut itm.stim[0], "read value {}", data as char);
+            if data == b'\r' {
+                iprintln!(&mut itm.stim[0], "got newline char");
+                buf.reverse();
+                break 'inner;
+            }
+            let res = buf.push(data);
+            if res.is_err() {
+                iprintln!(&mut itm.stim[0], "buffer full");
+                continue 'outer;
+            }
+        }
+        // send data back to receiver
+        echo.write(buf.iter().chain(&[b'\n', b'\r']));
     }
 
     // Original program before macro:
