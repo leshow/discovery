5ec52a00df04831030e184619dde8e84132ccc4e
Evan Cameron <cameron.evan@gmail.com>
2019-08-11 14:11:49 -0400

    implement uprintln macro

--- src/09-clocks-and-timers/src/main.rs
+++ src/09-clocks-and-timers/src/main.rs
@@ -1,18 +1,35 @@
 #![no_main]
 #![no_std]
 
-use aux9::{entry, tim6};
+use aux9::{entry, nop, tim6};
 
+// 8 Mhz is 8_000_000 cycles per second
+// so this must delay 1Khz * ms
 #[inline(never)]
 fn delay(tim6: &tim6::RegisterBlock, ms: u16) {
-    // TODO implement this
+    // set timer to go off after 'ms' ticks, 1 tick = 1 ms
+    tim6.arr.write(|w| w.arr().bits(ms));
+    // CEN: enables counter
+    tim6.cr1.modify(|_, w| w.cen().set_bit());
+    // busy wait on status register (sr)
+    while !tim6.sr.read().uif().bit_is_set() {}
+    tim6.sr.modify(|_, w| w.uif().clear_bit());
 }
 
 #[entry]
 fn main() -> ! {
     let (mut leds, rcc, tim6) = aux9::init();
 
-    // TODO initialize TIM6
+    // power on tim6 timer
+    rcc.apb1enr.modify(|_, w| w.tim6en().set_bit());
+    // OPM sets one-pulse-mode
+    // CEN disables counter (for now)
+    tim6.cr1.write(|w| w.opm().set_bit().cen().clear_bit());
+    // configer prescaler to have counter run @ 1 KHz
+    // APB1_CLOCK = 8 MHz
+    // apb / (psc + 1) = 1 KHz, psc = 7999
+    // makes the counter increase every millisecond
+    tim6.psc.write(|w| w.psc().bits(7_999));
 
     let ms = 50;
     loop {
--- src/11-usart/src/main.rs
+++ src/11-usart/src/main.rs
@@ -3,14 +3,75 @@
 #![no_std]
 
 #[allow(unused_imports)]
-use aux11::{entry, iprint, iprintln};
+use aux11::{entry, iprint, iprintln, usart1};
+use core::fmt::{self, Write};
+
+macro_rules! uprint {
+    ($serial:expr, $($arg:tt)*) => ($serial.write_fmt(format_args!($($arg)*)).ok());
+}
+
+macro_rules! uprintln {
+    ($serial:expr, $fmt:expr) => {
+        uprint!($serial, concat!($fmt, "\n"))
+    };
+    ($serial:expr, $fmt:expr, $($arg:tt)*) => {
+        uprint!($serial, concat!($fmt, "\n"), $($arg)*)
+    };
+}
+
+struct SerialPort {
+    usart1: &'static mut usart1::RegisterBlock,
+}
+
+impl Write for SerialPort {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        for c in s.bytes() {
+            while self.usart1.isr.read().txe().bit_is_clear() {}
+            self.usart1.tdr.write(|w| w.tdr().bits(u16::from(c)));
+        }
+        Ok(())
+    }
+}
 
 #[entry]
 fn main() -> ! {
-    let (usart1, mono_timer, itm) = aux11::init();
+    let (usart1, mono_timer, mut itm) = aux11::init();
 
-    // Send a single character
-    usart1.tdr.write(|w| w.tdr().bits(u16::from(b'X')));
+    let mut serial = SerialPort { usart1 };
+
+    uprintln!(serial, "Hello friend {}", 13 + 3);
+    // Original program before macro:
+    // while usart1.isr.read().txe().bit_is_clear() {}
+    // usart1.tdr.write(|w| w.tdr().bits(u16::from(b'\n')));
+    // let instant = mono_timer.now();
+
+    // // Send a single character
+    // let text = "The quick brown fox jumps over the lazy dog.";
+    // for &c in text.as_bytes() {
+    //     // The status register (ISR) has a flag (TXE) that indicates
+    //     // it's safe to write to TDR register
+    //     while usart1.isr.read().txe().bit_is_clear() {}
+    //     usart1.tdr.write(|w| w.tdr().bits(u16::from(c)));
+    // }
+
+    // let elapsed = instant.elapsed();
+
+    // iprintln!(
+    //     &mut itm.stim[0],
+    //     "for loop took {} ticks ({} us)",
+    //     elapsed,
+    //     elapsed as f32 / mono_timer.frequency().0 as f32 * 1e6
+    // );
+    // USART questions
+    // Execute your program inside the debugger, statement by statement. What do you see?
+    // It's able to write the string properly
+
+    // Then execute the program again but in one go using the continue command. What happens this time?
+    // after the first space character the bytes printed are not what I originally sent
+    // see: Thequc bon o jms vr helzydg
+
+    // Finally, build the program in release mode and, again, run it one go. What happens this time?
+    // It just prints one character "T"
 
     loop {}
 }
